43e90d1f2a15738c425930d79e6f24f5b01c11a5
Daniel Salgado <danielsalgado@unitec.edu>
2016-06-04 10:16:51 -0600

    Creacion de clase

---
+++ persona.cpp
@@ -0,0 +1,31 @@
+#include "person.h"
+#pragma once
+#include <string>
+#include <sstream>
+using std::string;
+using std::stringstream;
+
+Person::Person(string nombre,bool isMacho,char[] colorC,char[]colorO,colorP,bool fertil):nombre(nombre),isMacho(isMacho),colorC(colorC),colorO(colorO),colorP(colorP),fertil(fertil){
+}
+Person::~Person(){
+}
+
+string Person::toString()const{
+	stringstream ss;
+	ss << "Person: " << name << "Cabello: " << (colorC[0] == 'A' ? "Oscuro" : colorC[0] == 'a' ? "Rubio" : "Blonde" )  
+	<< "Ojos: " << (colorO[0] == 'A' ? "Oscuros" :  colorO[0] == 'a' ? "Verdes" : "Azules" )  
+	<< "Piel: " << (colorP[0] == 'A' ? "Oscura"  : 	"Blanca");
+	return ss.str();
+}
+
+const Person operator+(const Person& lhs, const Person& rhs){
+	Person retVal(lhs);
+	retVal += rhs;
+	return retVal;
+}
+
+const Person operator*(const Person& lhs, const Person& rhs){
+	Person retVal(lhs);
+	retVal *= rhs;
+	return retVal;
+}
